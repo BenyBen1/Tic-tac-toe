02040406f42622bf3e5883a493dc153e845aa89f
Ben Wachira <gishuks14@gmail.com>
2024-11-11 00:48:01 +0300

    npm added and weather description added in js

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 async function getWeather() {
     // Get the city name from the input field
     const cityName = document.getElementById('city').value;
-    const apiKey = "bb211d3d0e3ffb6aa101a71f6d562d37";  // Replace with your valid OpenWeather API key
+    const apiKey = "bb211d3d0e3ffb6aa101a71f6d562d37";  
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${apiKey}`;
 
     // Elements to display the temperature and weather icon
     const tempElement = document.getElementById('temp-div');
     const iconElement = document.getElementById('weather-icon');
+    const description = document.getElementById('description-div');
 
     try {
         // Fetch the data from the API
@@ -23,10 +24,12 @@ async function getWeather() {
         // Extract temperature in Celsius and weather icon
         const temperatureCelsius = Math.floor(result.main.temp);
         const iconCode = result.weather[0].icon;
+        const descriptions = result.weather[0].description
 
         // Display the temperature in the HTML and update the weather icon
         tempElement.textContent = `Temperature: ${temperatureCelsius}°C`;
         iconElement.src = `http://openweathermap.org/img/wn/${iconCode}.png`;
+        description.textContent = `Weather description: ${descriptions}`;
 
         // Show the icon by setting its display property to 'block'
         iconElement.style.display = 'block';
